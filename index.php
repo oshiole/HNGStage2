b0423581d3b41675bacc216c63466b42beb02f8c
Mubarak Yaqoub-Okponobi <31810586+oshiole@users.noreply.github.com>
2018-04-27 23:41:52 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" rel="stylesheet" text="text/css">
+    <title>HNGStage1</title>
+</head>
+<body>
+
+    <header>
+             <h1>HNG Intership 4 News.</h1>
+        <h4>Get your lastest news about the incredible intership here</h4>
+    </header>
+
+    <section class="wrapper">
+        <main class="content">
+                <article>
+                        <h1>HNG Intership 4 regristration closed.<hr></h1>
+                        <p>The well known HNG Intership 4.0 which is sponsored by hotels.ng owner Mark Essein had kicked up earlier this month and
+                            the regristration for the intership program which builds software developer both for beginners and advanced programmers
+                           looking to dive into the technology world has closed on the 21st April, 2017 with a total of 3083 developer registered
+                            across Africa.
+                        </p>
+            </article>
+
+            <article>
+                            <h1>HNG Intership 4 begins work.<hr> </h1>
+                            <p>The intership program kicked off with project staging from #stage1 to #stage5 to be completed by interns and reviewed by
+                                mentors who help you write, improve and create real-life project. Each stages must be completed with the appropriate 
+                            tools required and submitted for review before moving into the next stage. </p>
+                </article>
+        
+        
+                <article>
+                            <h1>HNG Intership 4 intern given 2-weeks deadline for project.<hr></h1>
+                            <p>Mark Essein announced on the slack page that every intern must complete #stage1 and #stage2 before the next 14 days or
+                                that intern is liable to being removed from the intership. It has increased the amount of developers across the platform
+                                trying their best to complete each stages to avoid removal.
+                            </p>
+                </article>
+        
+        </main>
+       
+        <aside class="aside">
+            <?php
+            echo "Today date is ". date("<b>l, d / m / Y</b>") . "<br />";
+            echo "The time is " . date("<b>h : ia</b>");
+            ?>
+        </aside>
+    </section>
+    
+
+    <footer class="footer">
+        <cite>Designed by Coding_Imran</cite>
+    </footer>
+</body>
+</html>
